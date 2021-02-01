5a39daaa686526cd1486a3f4d58ac751a4ec10ec
Biniyam T <biniyamwonde135@gmail.com>
2021-02-01 16:43:35 +0300

    Update: Improve performance

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
+
+
+// Updated: 2025-11-24
