397bcd9b9efdf9ef8d657bfd87119af6e17f3499
Biniyam T <biniyamwonde135@gmail.com>
2021-02-03 10:42:29 +0300

    Refactor: Improve code structure

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+// Application constants
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.example.com';
+export const APP_NAME = 'Trust Rent';
+export const VERSION = '1.0.0';
