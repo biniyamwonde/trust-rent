245da6583709cf8adad49664c7732601c1de8578
Biniyam T <biniyamwonde135@gmail.com>
2021-02-07 16:21:43 +0300

    Fix: Resolve state management issue

---
+++ src/lib/validation.ts
@@ -0,0 +1,9 @@
+// Validation utilities
+export function validateEmail(email: string): boolean {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+export function validatePhone(phone: string): boolean {
+  return /^\+?[\d\s-()]+$/.test(phone);
+}
