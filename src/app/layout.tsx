f24d6ba38ce51aad44a9febdea4c6014bc7f2002
Biniyam T <biniyamwonde135@gmail.com>
2021-02-07 12:03:02 +0300

    Refactor: Simplify component logic

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Noto_Sans_TC } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const notoSansTC = Noto_Sans_TC({
+  variable: "--font-noto-sans-tc",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Trust Rent - Thai Rental Platform",
+  description: "Fast, secure, and flexible rent payments — all in one place.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${notoSansTC.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+
+// TODO: Review implementation
