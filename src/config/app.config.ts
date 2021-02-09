83025a3b4f11658f419ad098c507fa20be201b7d
Biniyam T <biniyamwonde135@gmail.com>
2021-02-09 16:56:29 +0300

    Feat: Implement user dashboard

---
+++ src/config/app.config.ts
@@ -0,0 +1,7 @@
+// Application configuration
+export const appConfig = {
+  name: 'Trust Rent',
+  version: '1.0.0',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  environment: process.env.NODE_ENV,
+};
