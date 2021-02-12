a3d78907ff048d2e3a610aace974e003834bf542
Biniyam T <biniyamwonde135@gmail.com>
2021-02-12 18:25:27 +0300

    Feat: Add payment integration

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,5 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.example.com';
 export const APP_NAME = 'Trust Rent';
 export const VERSION = '1.0.0';
+
+// TODO: Review implementation
