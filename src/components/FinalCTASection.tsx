815b640bd64088854852dc8ab0c0c24375f8f0db
Biniyam T <biniyamwonde135@gmail.com>
2021-02-25 14:32:53 +0300

    Fix: Resolve styling inconsistencies

---
+++ src/components/FinalCTASection.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+const img =  "/assets/mstercard-girl.png";
+const img67A9F58C1Cefb71E5708588AScbE5B7A5E4Bd9CE58D80E59F9F2011 = "/assets/building.png";
+
+export default function FinalCTASection() {
+  return (
+    <div className="bg-[#ff5552] box-border content-stretch flex gap-2.5 items-start justify-start pb-[120px] pt-16 px-20 relative w-full" data-node-id="2:3011">
+      <div className="absolute content-stretch flex gap-5 h-[489px] items-start justify-start left-[512px] top-16" data-node-id="2:3012">
+        <div className="bg-white h-[489px] relative shadow-[0px_0px_20px_0px_rgba(51,51,51,0.1)] shrink-0 w-[414px] rounded-[24px] overflow-hidden">
+          <div className="content-stretch flex flex-col h-[233px] items-start justify-start relative shrink-0 w-full">
+            <div className="bg-center bg-cover bg-no-repeat h-[233px] rounded-tl-[16px] rounded-tr-[24px] shrink-0 w-full" style={{ backgroundImage: `url('${img}')` }} />
+          </div>
+          <div className="bg-white box-border content-stretch flex flex-col gap-[29px] items-start justify-start px-5 py-6 relative rounded-bl-[24px] rounded-br-[24px] shrink-0 w-full">
+            <div className="content-stretch flex flex-col gap-6 items-start justify-start relative shrink-0 w-full">
+              <div className="content-stretch flex flex-col gap-8 items-start justify-end relative shrink-0 w-full">
+                <div className="content-stretch flex flex-col gap-2 items-start justify-start leading-[0] relative shrink-0 text-[24px] w-full">
+                  <div className="font-['Noto_Sans_TC:Bold',_sans-serif] font-bold relative shrink-0 text-[#ff5552] text-nowrap">
+                    <p className="leading-[32px] whitespace-pre">Mastercard</p>
+                  </div>
+                  <div className="font-['Noto_Sans_TC:Regular',_sans-serif] font-normal min-w-full relative shrink-0 text-[#333333]" style={{ width: "min-content" }}>
+                    <p className="leading-[32px]">Renting made easy — pay deposits, rent, and commissions with Mastercard.</p>
+                  </div>
+                </div>
+              </div>
+              <button className="box-border content-stretch flex gap-1 h-12 items-center justify-center px-4 py-3 relative rounded-[120px] shrink-0 border border-[#ff5552] border-solid hover:bg-[#ff5552] hover:text-white transition-all duration-200">
+                <div className="content-stretch flex gap-2 items-center justify-center relative shrink-0">
+                  <div className="font-['Noto_Sans_TC:Medium',_sans-serif] font-medium leading-[0] relative shrink-0 text-[#ff5552] text-[20px] text-nowrap tracking-[0.4px] hover:text-white">
+                    <p className="leading-[24px] whitespace-pre">Learn More</p>
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="bg-white h-[489px] relative shadow-[0px_0px_20px_0px_rgba(51,51,51,0.1)] shrink-0 w-[414px] rounded-[24px] overflow-hidden">
+          <div className="content-stretch flex flex-col h-[233px] items-start justify-start relative shrink-0 w-full">
+            <div className="bg-center bg-cover bg-no-repeat h-[233px] rounded-tl-[16px] rounded-tr-[24px] shrink-0 w-full" style={{ backgroundImage: `url('${img67A9F58C1Cefb71E5708588AScbE5B7A5E4Bd9CE58D80E59F9F2011}')` }} />
+          </div>
+          <div className="bg-white box-border content-stretch flex flex-col gap-[29px] items-start justify-start px-5 py-6 relative rounded-bl-[24px] rounded-br-[24px] shrink-0 w-full">
+            <div className="content-stretch flex flex-col gap-6 items-start justify-start relative shrink-0 w-full">
+              <div className="content-stretch flex flex-col gap-8 items-start justify-end relative shrink-0 w-full">
+                <div className="content-stretch flex flex-col gap-2 items-start justify-start leading-[0] relative shrink-0 text-[24px] w-full">
+                  <div className="font-['Noto_Sans_TC:Bold',_sans-serif] font-bold relative shrink-0 text-[#ff5552] text-nowrap">
+                    <p className="leading-[32px] whitespace-pre">Standard Chartered</p>
+                  </div>
+                  <div className="font-['Noto_Sans_TC:Regular',_sans-serif] font-normal min-w-full relative shrink-0 text-[#333333]" style={{ width: "min-content" }}>
+                    <p className="leading-[32px]">用渣打國泰Mastercard®交租免首次服務費 送高達5,000里+抽獎</p>
+                  </div>
+                </div>
+              </div>
+              <button className="box-border content-stretch flex gap-1 h-12 items-center justify-center px-4 py-3 relative rounded-[120px] shrink-0 border border-[#ff5552] border-solid hover:bg-[#ff5552] hover:text-white transition-all duration-200">
+                <div className="content-stretch flex gap-2 items-center justify-center relative shrink-0">
+                  <div className="font-['Noto_Sans_TC:Medium',_sans-serif] font-medium leading-[0] relative shrink-0 text-[#ff5552] text-[20px] text-nowrap tracking-[0.4px] hover:text-white">
+                    <p className="leading-[24px] whitespace-pre">了解更多</p>
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="grid-cols-[max-content] grid-rows-[max-content] inline-grid leading-[0] place-items-start relative shrink-0">
+        <div className="box-border content-stretch flex flex-col gap-6 items-start justify-start ml-0 mt-0 relative text-white">
+          <div className="font-['Mulish:Bold',_sans-serif] font-bold relative shrink-0 text-[48px] tracking-[0.96px] w-[370px]">
+            <p className="leading-[60px]">
+              Deposits, First Month&apos;s Rent, Agent Commissions Can All Be Paid by Card!
+            </p>
+          </div>
+          <div className="font-['Noto_Sans_TC:Regular',_sans-serif] font-normal min-w-full relative shrink-0 text-[24px]" style={{ width: "min-content" }}>
+            <p className="leading-[32px]">Monthly rental expenses turn into cashback and rewards</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+// Last updated: 2025-11-24
