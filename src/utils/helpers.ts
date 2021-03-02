777b90cfa1f25e988c4487f106e6a9490cb42f2f
Biniyam T <biniyamwonde135@gmail.com>
2021-03-02 13:50:36 +0300

    Update: Improve performance

---
+++ src/utils/helpers.ts
@@ -0,0 +1,8 @@
+// Utility helper functions
+export function formatDate(date: Date): string {
+  return date.toLocaleDateString();
+}
+
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
+}
