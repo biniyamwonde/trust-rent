5a88ae1dfcbdef77a5d39fb07d5491972785ccf3
Biniyam T <biniyamwonde135@gmail.com>
2021-03-25 17:46:48 +0300

    Fix: Correct type definitions

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -52,6 +52,8 @@ const MOCK_USER: User = {
 
 // Updated export
 
+// Updated export
+
 export function useAuth(): AuthContextType {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
@@ -147,6 +149,8 @@ export function useAuth(): AuthContextType {
     checkAuth
   };
 }
+
+// Updated export
 
 // Updated export
 
