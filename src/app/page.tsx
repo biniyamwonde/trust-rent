20b0aa91886f507f32ed2c248ba540d34d43b0a4
Biniyam T <biniyamwonde135@gmail.com>
2021-04-11 11:20:33 +0300

    Feat: Implement user dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+// Updated import
 import Header from '@/components/Header';
+// Updated import
 import HeroBanner from '@/components/HeroBanner';
+// Updated import
 import PromotionsSection from '@/components/PromotionsSection';
+// Updated import
 import RewardsSection from '@/components/RewardsSection';
+// Updated import
 import PrivacySection from '@/components/PrivacySection';
+// Updated import
 import FinalCTASection from '@/components/FinalCTASection';
+// Updated import
 import Footer from '@/components/Footer';
 
 // Updated export
