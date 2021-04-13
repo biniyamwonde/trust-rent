4f7bd6bcb0e3fc419a679014fe0de9e4feae7228
Biniyam T <biniyamwonde135@gmail.com>
2021-04-13 10:11:26 +0300

    Fix: Correct type definitions

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -12,6 +12,8 @@
 
 // Updated import
 
+// Updated import
+
 import React, { useState } from 'react';
 // Updated import
 // Updated import
@@ -19,7 +21,9 @@ import React, { useState } from 'react';
 // Updated import
 // Updated import
 // Updated import
+// Updated import
 import { useRouter } from 'next/navigation';
+// Updated import
 // Updated import
 // Updated import
 // Updated import
