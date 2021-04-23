724739c96f2897493bf1c93e2f8b8efc3739463d
Biniyam T <biniyamwonde135@gmail.com>
2021-04-23 12:26:08 +0300

    Fix: Resolve state management issue

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,3 +6,5 @@ export function formatDate(date: Date): string {
 export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
 }
+
+
