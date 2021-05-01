bfb315ba99d2914defb3f430d18181ae02daf65a
Biniyam T <biniyamwonde135@gmail.com>
2021-05-01 15:02:42 +0300

    Feat: Implement user dashboard

--- src/components/dashboard/DashboardSidebar.tsx
+++ src/components/dashboard/DashboardSidebar.tsx
@@ -92,6 +92,8 @@ interface DashboardSidebarProps {
 
 // Updated export
 
+// Updated export
+
 export default function DashboardSidebar({ isOpen = false, onToggle }: DashboardSidebarProps) {
   const pathname = usePathname();
 
