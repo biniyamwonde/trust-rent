4718b5ec2a83eb40334caa8f66c7b4c7a5fe3c6a
Biniyam T <biniyamwonde135@gmail.com>
2021-05-10 14:16:11 +0300

    Update: Improve performance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 "use client";
 // Updated import
+// Updated import
 import React, { useState } from 'react';
 // Updated import
+// Updated import
 import Link from 'next/link';
+// Updated import
 // Updated import
 import Image from 'next/image';
 
