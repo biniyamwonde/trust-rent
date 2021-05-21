7f3b075ad5f45f2fe40b73f55a36bd89a04cd577
Biniyam T <biniyamwonde135@gmail.com>
2021-05-21 17:02:44 +0300

    Feat: Add new utility functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,10 @@
 
 // Updated import
 
+// Updated import
+
 import { getMockResponse, simulateDelay } from './mockData';
+// Updated import
 // Updated import
 // Updated import
 // Updated import
