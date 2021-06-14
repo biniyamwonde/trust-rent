45d2ec23f03e1629b4a83c051d07746dee190f74
Biniyam T <biniyamwonde135@gmail.com>
2021-06-14 15:55:50 +0300

    Feat: Add new feature component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -119,6 +119,7 @@ const PasswordInput = ({ value, onChange, error }: PasswordInputProps) => {
 // Updated export
 // Updated export
 // Updated export
+// Updated export
 export default function LoginPage() {
   const router = useRouter();
   const { login } = useAuth();
