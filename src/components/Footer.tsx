8568569284da05daec07e1d61a434cd9b404dda0
Biniyam T <biniyamwonde135@gmail.com>
2021-06-27 11:31:31 +0300

    Feat: Implement search functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,6 +22,8 @@ const img4 = "/assets/arrow.svg";
 
 // Updated export
 
+// Updated export
+
 export default function Footer() {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
